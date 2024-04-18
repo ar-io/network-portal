4bdd7d7b151563646d83439f7c9e89d9f735fc1b
Atticus <atticus@macbook36gb.local>
2024-04-18 11:54:24 -0600

    fix(logger): use base winston logger

--- src/constants.ts
+++ src/constants.ts
@@ -1,66 +1,14 @@
-import winston, { createLogger, format, transports } from 'winston';
+import { createLogger, format, transports } from 'winston';
 
-import { Logger } from '../types/index.js';
 
 export const THEME_TYPES = {
   LIGHT: 'light',
   DARK: 'dark',
 };
 
-export class DefaultLogger implements Logger {
-  private logger: winston.Logger;
-  constructor({
-    level = 'info',
-    logFormat = 'simple',
-  }: {
-    level?: 'info' | 'debug' | 'error' | 'none' | undefined;
-    logFormat?: 'simple' | 'json' | undefined;
-  } = {}) {
-    this.logger = createLogger({
-      level,
-      silent: level === 'none',
-      format: getLogFormat(logFormat),
+export const defaultLogger = createLogger({
+      level: 'info',
+      silent: false,
+      format: format.simple(),
       transports: [new transports.Console()],
-    });
-  }
-
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  info(message: string, ...args: any[]) {
-    this.logger.info(message, ...args);
-  }
-
-  warn(message: string, ...args: any[]) {
-    this.logger.warn(message, ...args);
-  }
-
-  error(message: string, ...args: any[]) {
-    this.logger.error(message, ...args);
-  }
-
-  debug(message: string, ...args: any[]) {
-    this.logger.debug(message, ...args);
-  }
-
-  setLogLevel(level: string) {
-    this.logger.level = level;
-  }
-
-  setLogFormat(logFormat: string) {
-    this.logger.format = getLogFormat(logFormat);
-  }
-  /* eslint-enable @typescript-eslint/no-explicit-any */
-}
-
-function getLogFormat(logFormat: string) {
-  return format.combine(
-    format((info) => {
-      if (info.stack && info.level !== 'error') {
-        delete info.stack;
-      }
-      return info;
-    })(),
-    format.errors({ stack: true }), // Ensure errors show a stack trace
-    format.timestamp(),
-    logFormat === 'json' ? format.json() : format.simple(),
-  );
-}
+    })
--- types/common.ts
+++ types/common.ts
@@ -1,15 +1,6 @@
 import { Readable } from 'stream';
 import { ReadableStream } from 'stream/web';
 
-export interface Logger {
-  setLogLevel: (level: string) => void;
-  setLogFormat: (logFormat: string) => void;
-  info: (message: string, ...args: any[]) => void;
-  warn: (message: string, ...args: any[]) => void;
-  error: (message: string, ...args: any[]) => void;
-  debug: (message: string, ...args: any[]) => void;
-}
-
 export interface HTTPClient {
   get<T>({
     endpoint,

0d99b0a8538fa0b71807c2c9dfe627bc2f6ab14d
Steven Yi <stevenyi@gmail.com>
2024-04-19 15:39:40 -0400

    fix: fill in content param for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 import { colors } from './tokens';
 
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: 'class', // or 'media' or 'class
   theme: {
     extend: {

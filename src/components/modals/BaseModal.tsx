cffcff843d7cf541f164fcb0516d992f2265b816
Steven Yi <stevenyi@gmail.com>
2024-05-01 09:50:06 -0400

    fix: disable closing dialogs with escape key and outside dialog mouse presses

--- src/components/modals/BaseModal.tsx
+++ src/components/modals/BaseModal.tsx
@@ -12,14 +12,14 @@ const BaseModal = ({
   children: ReactElement;
 }) => {
   return (
-    <Dialog open={open} onClose={onClose} className="relative z-50">
+    <Dialog open={open} onClose={() => {}} className="relative z-50">
       <div
         className="fixed inset-0 w-screen bg-neutrals-1100/80"
         aria-hidden="true"
       />
 
       <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-        <Dialog.Panel className="relative w-full max-w-sm items-stretch rounded-[12px] bg-[#111112] p-[32px] text-center text-grey-100">
+        <Dialog.Panel className="relative items-stretch rounded-[12px] bg-[#111112] p-[32px] text-center text-grey-100">
           <button className="absolute right-[-28px] top-0" onClick={onClose}>
             <CloseIcon />
           </button>

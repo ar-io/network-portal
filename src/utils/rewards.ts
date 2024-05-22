807c6159fde1ba6e76bfab4a0f076982a89a6f7a
Steven Yi <stevenyi@gmail.com>
2024-05-22 17:36:43 -0400

    feat: utility code for calculating gateway and user rewards (EEY, EAY)

---
+++ src/utils/rewards.ts
@@ -0,0 +1,58 @@
+import { Gateway, IOToken, mIOToken } from '@ar.io/sdk/web';
+
+const EPOCHS_PER_YEAR = 52;
+const EPOCH_DISTRIBUTION_RATIO = 0.0025; // 0.25%
+const GATEWAY_REWARDS_RATIO = 0.95; // 95%
+// const OBSERVER_REWARDS_RATIO = .05; // 5%
+
+export interface GatewayRewards {
+  totalDelegatedStake: IOToken;
+  rewardsSharedPerEpoch: IOToken;
+  EEY: number;
+  EAY: number;
+}
+
+export const calculateGatewayRewards = (
+  protocolBalance: IOToken,
+  totalGateways: number,
+  gateway: Gateway,
+): GatewayRewards => {
+  const epochRewards = protocolBalance.valueOf() * EPOCH_DISTRIBUTION_RATIO;
+
+  const baseGatewayReward =
+    (epochRewards * GATEWAY_REWARDS_RATIO) / totalGateways;
+
+  const gatewayRewardShareRatio = gateway.settings.delegateRewardShareRatio;
+  const totalDelegatedStake = new mIOToken(gateway.totalDelegatedStake)
+    .toIO();
+
+  const rewardsSharedPerEpoch = new IOToken(baseGatewayReward * gatewayRewardShareRatio);
+
+  const EEY = rewardsSharedPerEpoch.valueOf() / totalDelegatedStake.valueOf();
+  const EAY = EEY * EPOCHS_PER_YEAR;
+
+  return {
+    totalDelegatedStake,
+    rewardsSharedPerEpoch,
+    EEY,
+    EAY,
+  };
+};
+
+export const calculateUserRewards = (
+  gatewayRewards: GatewayRewards,
+  userDelegatedStake: IOToken,
+) => {
+  const delegatedStake = userDelegatedStake.valueOf();
+  const stakeProportion =
+    delegatedStake / (gatewayRewards.totalDelegatedStake.valueOf() + delegatedStake);
+  const epochReward = gatewayRewards.rewardsSharedPerEpoch.valueOf() * stakeProportion;
+
+  const EEY = epochReward / delegatedStake;
+  const EAY = EEY * EPOCHS_PER_YEAR;
+
+  return {
+    EEY,
+    EAY,
+  };
+};

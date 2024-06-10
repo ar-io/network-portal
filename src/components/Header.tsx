62aa1e0fee3e56820c21adf98939aa5146301bfd
Steven Yi <stevenyi@gmail.com>
2024-06-10 13:11:50 -0400

    fix: defend against undefined epochIndex

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
     <header className="mt-[24px] flex h-[72px] rounded-[12px] border py-[16px] pl-[24px] pr-[16px] leading-[1.4] dark:border-transparent-100-8 dark:bg-grey-1000 dark:text-grey-300">
       <div className="inline-flex h-[38px] flex-col items-start justify-start gap-1 border-r pr-6 dark:border-transparent-100-8">
         <div className="text-[12px] text-high">
-          {currentEpoch
+          {currentEpoch?.epochIndex
             ? currentEpoch.epochIndex.toLocaleString('en-US')
             : NBSP}
         </div>

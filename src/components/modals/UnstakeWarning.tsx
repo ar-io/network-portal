990e540ff452055b45c6e32ece484c3bbde6c948
Steven Yi <stevenyi@gmail.com>
2024-06-12 15:03:24 -0400

    fix: updated text

--- src/components/modals/UnstakeWarning.tsx
+++ src/components/modals/UnstakeWarning.tsx
@@ -6,8 +6,8 @@ const UnstakeWarning = () => {
       <div className="flex gap-[12px] rounded bg-containerL3 p-[16px]">
         <WarningIcon width={40} height={20} />
         <div className="grow text-[13px] text-high">
-          30 days is the standard unstaking period. During this time your tokens
-          will be locked and will not be accruing rewards.
+          30 days is the standard unstaking period. During this withdrawal time
+          your tokens will be locked and will not be accruing rewards.
         </div>
       </div>
     </div>

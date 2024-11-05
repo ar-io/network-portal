715e1a390107bf87b387c512ecfe91c35624929b
Steven Yi <stevenyi@gmail.com>
2024-11-05 14:00:03 -0500

    feat: add observers button to dashboard observers card

--- src/pages/Dashboard/ObserverPerformancePanel.tsx
+++ src/pages/Dashboard/ObserverPerformancePanel.tsx
@@ -1,7 +1,11 @@
+import Button from '@src/components/Button';
+import { BinocularsIcon } from '@src/components/icons';
 import Placeholder from '@src/components/Placeholder';
 import { useGlobalState } from '@src/store';
+import { useNavigate } from 'react-router-dom';
 
 const ObserverPerformancePanel = () => {
+  const navigate = useNavigate();
   const currentEpoch = useGlobalState((state) => state.currentEpoch);
 
   const reportsCount = currentEpoch
@@ -10,7 +14,19 @@ const ObserverPerformancePanel = () => {
 
   return (
     <div className="min-w-[22rem] rounded-xl border border-grey-500 px-6 py-5">
-      <div className=" text-sm text-mid">Observer Performance</div>
+      <div className="flex w-full items-center">
+        <div className="grow text-sm text-mid">Observer Performance</div>
+        <Button
+          className="max-h-6 text-xs"
+          icon={<BinocularsIcon className="size-3" />}
+          title={'Observers'}
+          text={'Observers'}
+          active={true}
+          onClick={() => {
+            navigate('/observers');
+          }}
+        />
+      </div>
       <div className="grow"></div>
       <div className="flex h-full justify-between pb-5 align-bottom text-[2.625rem] font-bold text-high">
         <div className="flex flex-col place-items-end">
@@ -25,7 +41,7 @@ const ObserverPerformancePanel = () => {
         </div>
         <div className="flex flex-col place-items-end text-right text-xs">
           <div className="grow" />
-          {reportsCount !== undefined? (
+          {reportsCount !== undefined ? (
             <>
               <div>{reportsCount}/50</div>
               <div>observations submitted</div>

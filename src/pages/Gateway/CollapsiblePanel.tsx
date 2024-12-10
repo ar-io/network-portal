0ec94aacc9eabcb105a44557ed2e6e663ccbc640
Steven Yi <stevenyi@gmail.com>
2024-12-09 23:30:55 -0500

    fix: make text and icon all part of button trigger for collapsing/expanding panel

--- src/pages/Gateway/CollapsiblePanel.tsx
+++ src/pages/Gateway/CollapsiblePanel.tsx
@@ -19,17 +19,21 @@ const CollapsiblePanel = ({
       <div
         className={`flex items-center gap-2 border-b border-grey-500 bg-containerL3 px-6 py-4 ${isOpen ? 'rounded-t-xl' : 'rounded-xl'}`}
       >
-        {children && (
-          <button onClick={() => setIsOpen(!isOpen)}>
+        <div className="grow">
+        {children ? (
+          <button className="flex gap-2" onClick={() => setIsOpen(!isOpen)}>
             {isOpen ? (
               <Minus className="size-5" />
             ) : (
               <CirclePlus className="size-5" />
             )}
+            <div>{title}</div>
           </button>
+        ) : (
+          <div className="items-center whitespace-nowrap text-high">
+            {title}
+          </div>
         )}
-        <div className="grow items-center whitespace-nowrap text-high">
-          {title}
         </div>
         {titleRight}
       </div>

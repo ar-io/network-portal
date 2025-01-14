1cb4b23173ce10e12298a9d4faebb8430f08e375
dtfiedler <dylan@ar.io>
2025-01-14 08:59:03 -0600

    chore(deps): update @ar.io/sdk deps
    The new version allows for fetching historical epoch data > 14 epochs. This parameter is hard coded, but could now be exposed on the frontend

--- src/hooks/useEpochs.ts
+++ src/hooks/useEpochs.ts
@@ -2,7 +2,10 @@ import { useGlobalState } from '@src/store';
 import { getEpoch } from '@src/store/db';
 import { useQuery } from '@tanstack/react-query';
 
-/** Returns last 14 epochs */
+// TODO: this could be a parameter provided by the user now
+const HISTORICAL_EPOCHS_TO_FETCH = 13;
+
+/** Returns last EPOCHS_TO_FETCH epochs */
 const useEpochs = () => {
   const arIOReadSDK = useGlobalState((state) => state.arIOReadSDK);
   const startEpoch = useGlobalState((state) => state.currentEpoch);
@@ -15,11 +18,11 @@ const useEpochs = () => {
       }
 
       const additionalEpochs = await Promise.all(
-        Array.from({ length: 13 }, (_, index) => startEpoch.epochIndex - index - 1)
+        Array.from({ length: HISTORICAL_EPOCHS_TO_FETCH }, (_, index) => startEpoch.epochIndex - index - 1)
           .map(epochIndex => getEpoch(arIOReadSDK, epochIndex))
       );      
 
-      return [startEpoch, ...additionalEpochs.filter(e => e !== undefined)];
+      return [startEpoch, ...additionalEpochs.filter((e: any) => e !== undefined)];
     },
   });
 

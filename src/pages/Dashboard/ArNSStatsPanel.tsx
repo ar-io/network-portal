f751031a9636cccf8b397560fea9404c464d7c3d
Steven Yi <stevenyi@gmail.com>
2025-02-04 16:43:25 -0500

    feat(dashboard): add link to ArNS.app and adjust layout

--- src/pages/Dashboard/ArNSStatsPanel.tsx
+++ src/pages/Dashboard/ArNSStatsPanel.tsx
@@ -1,3 +1,4 @@
+import Button from '@src/components/Button';
 import Placeholder from '@src/components/Placeholder';
 import Tooltip from '@src/components/Tooltip';
 import useArNSStats from '@src/hooks/useArNSStats';
@@ -10,27 +11,38 @@ const ArNSStatsPanel = () => {
   return (
     <div className="flex min-w-[22rem] flex-col rounded-xl border border-grey-500 px-6 py-5">
       <div className="flex items-center">
-        <div className="grow text-sm text-mid">ArNS Names Purchased</div>
-
-        {arnsStats && (
-          <Tooltip
-            message={
-              <div>
-                <div className="mb-2 font-semibold text-high">ArNS Stats</div>
+        <div className="flex grow items-center gap-2 text-sm text-mid">
+          ArNS Names Purchased
+          {arnsStats && (
+            <Tooltip
+              message={
                 <div>
-                  <div>Active Names: {arnsStats.totalActiveNames}</div>
-                  <div>Returned Names: {arnsStats.totalReturnedNames}</div>
+                  <div className="mb-2 font-semibold text-high">ArNS Stats</div>
                   <div>
-                    Grace Period Names: {arnsStats.totalGracePeriodNames}
+                    <div>Active Names: {arnsStats.totalActiveNames}</div>
+                    <div>Returned Names: {arnsStats.totalReturnedNames}</div>
+                    <div>
+                      Grace Period Names: {arnsStats.totalGracePeriodNames}
+                    </div>
                   </div>
-                  <div>Reserved Names: {arnsStats.totalReservedNames}</div>
                 </div>
-              </div>
-            }
-          >
-            <InfoIcon className="size-[1.125rem] text-low" />
-          </Tooltip>
-        )}
+              }
+            >
+              <InfoIcon className="size-[1.125rem] text-low" />
+            </Tooltip>
+          )}
+        </div>
+
+        <Button
+          className="max-h-6 text-xs"
+          // icon={<BinocularsIcon className="size-3" />}
+          title={'ArNS.app'}
+          text={'Open ArNS app'}
+          active={true}
+          onClick={() => {
+            window.open('https://arns.app', '_blank', 'noopener,noreferrer');
+          }}
+        />
       </div>
       <div className="self-center px-24 py-6 text-center text-[2.625rem]">
         {arnsStats ? (

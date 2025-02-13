f71391052e48eb612593b480acb4ce7cdb34748b
kunstmusik <kunstmusik@users.noreply.github.com>
2025-02-13 18:25:58 +0000

    deploy: 45267b57100ee3da73b8c969f08ade65c8731d30

--- assets/Dropdown-B6-YeINk.js
+++
@@ -1,8 +0,0 @@
-import{e as a,j as n}from"./index-CMpwR7Tm.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},r=new Error().stack;r&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[r]="e71bd9c6-bc3b-44fd-bc20-2608e242fbfb",e._sentryDebugIdIdentifier="sentry-dbid-e71bd9c6-bc3b-44fd-bc20-2608e242fbfb")}catch{}})();/**
- * @license lucide-react v0.461.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const i=a("ChevronDown",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]),l=({options:e,onChange:r,value:t,tightPadding:o=!1})=>n.jsxs("div",{className:"relative w-fit min-w-fit",children:[n.jsx("select",{className:`cursor-pointer appearance-none rounded-xl bg-transparent ${!o&&"py-4 pl-4"} pr-10 text-mid outline-none`,onChange:r,value:t,children:e.map((s,d)=>n.jsx("option",{value:s.value,children:s.label},d))}),n.jsx("div",{className:`pointer-events-none absolute right-2 top-1/2 -translate-y-1/2 rounded-lg border border-grey-1100 bg-containerL3 bg-gradient-to-b 
-     from-[rgba(102,102,102,0.06)] to-[rgba(0,0,0,0.06)] p-1 shadow-inner  shadow-grey-600`,children:n.jsx(i,{className:"pointer-events-none size-4  text-mid"})})]});export{l as D};
-//# sourceMappingURL=Dropdown-B6-YeINk.js.map

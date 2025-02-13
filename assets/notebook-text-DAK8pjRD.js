cf4a64041bac54ce0140fd5059c2bd090f40793d
kunstmusik <kunstmusik@users.noreply.github.com>
2025-02-13 22:12:02 +0000

    deploy: 606200a0accb3c6ede10e64febb2ec8b8425358b

---
+++ assets/notebook-text-DAK8pjRD.js
@@ -0,0 +1,7 @@
+import{e as d}from"./index-D56c_T-o.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},t=new Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]="36e8529d-5f4f-4bab-87b3-a710fa1880ec",e._sentryDebugIdIdentifier="sentry-dbid-36e8529d-5f4f-4bab-87b3-a710fa1880ec")}catch{}})();/**
+ * @license lucide-react v0.461.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=d("NotebookText",[["path",{d:"M2 6h4",key:"aawbzj"}],["path",{d:"M2 10h4",key:"l0bgd4"}],["path",{d:"M2 14h4",key:"1gsvsf"}],["path",{d:"M2 18h4",key:"1bu2t1"}],["rect",{width:"16",height:"20",x:"4",y:"2",rx:"2",key:"1nb95v"}],["path",{d:"M9.5 8h5",key:"11mslq"}],["path",{d:"M9.5 12H16",key:"ktog6x"}],["path",{d:"M9.5 16H14",key:"p1seyn"}]]);export{n as N};
+//# sourceMappingURL=notebook-text-DAK8pjRD.js.map

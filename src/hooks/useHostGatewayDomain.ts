8aa6c61a208adb546e30c3704109358ecc49786d
Steven Yi <stevenyi@gmail.com>
2025-02-17 16:43:31 -0500

    chore: optimize to not retry as only one fetch attempt necessary here

--- src/hooks/useHostGatewayDomain.ts
+++ src/hooks/useHostGatewayDomain.ts
@@ -1,9 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import ky from 'ky';
 
-/** Checks where page is served and uses /ar-io/info endpoint to check if it is a gateway. 
+/** Checks where page is served and uses /ar-io/info endpoint to check if it is a gateway.
  *
- * @returns domain name if it is a gateway, otherwise null 
+ * @returns domain name if it is a gateway, otherwise null
  */
 const useHostGatewayDomain = () => {
   const queryResults = useQuery({
@@ -14,8 +14,10 @@ const useHostGatewayDomain = () => {
       const domain = parts.length > 1 ? parts.slice(1).join('.') : parts[0];
 
       try {
-        const response = await ky.get(`https://${domain}/ar-io/info`);
-        const data = await response.json() as any;
+        const response = await ky.get(`https://${domain}/ar-io/info`, {
+          retry: 0,
+        });
+        const data = (await response.json()) as any;
         if (data?.wallet) {
           return domain;
         } else {

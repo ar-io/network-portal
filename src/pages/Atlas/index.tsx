ed97d4cf6726bfbbcbd0228cda9e8c9607efb962
Michael Buhler <michael@michaelbuhler.com>
2025-07-08 15:08:44 +0700

    feat(atlas): page/nav scaffolding

---
+++ src/pages/Atlas/index.tsx
@@ -0,0 +1,12 @@
+import Header from '@src/components/Header';
+
+const Atlas = () => {
+  return (
+    <div className="flex max-w-full flex-col gap-6">
+      <Header />
+      Hello world
+    </div>
+  )
+};
+
+export default Atlas;

39f80256a5f2a72988c5d098078b27e91d5808b5
Michael Buhler <michael@michaelbuhler.com>
2025-07-08 21:51:55 +0700

    feat(atlas): populate table with staking info

--- src/pages/Atlas/index.tsx
+++ src/pages/Atlas/index.tsx
@@ -1,10 +1,128 @@
+import type { CellContext, ColumnDef } from '@tanstack/react-table';
+
+import { mARIOToken } from '@ar.io/sdk/web';
+import { createColumnHelper } from '@tanstack/react-table';
+import { useEffect, useMemo, useState } from 'react';
+
+import CopyButton from '@src/components/CopyButton';
 import Header from '@src/components/Header';
+import TableView from '@src/components/TableView';
+import useGateways from '@src/hooks/useGateways';
+
+interface RowData {
+  label: string;
+  domain: string;
+  operatorStake: number;
+  delegatedStake: number;
+  totalStake: number;
+}
+
+const STAKE_FORMAT = new Intl.NumberFormat('en-US', {
+  style: 'decimal',
+  minimumFractionDigits: 1,
+  maximumFractionDigits: 1,
+});
+const formatStake = (stake: number) => {
+  return STAKE_FORMAT.format(stake);
+}
+const renderStake = (ctx: CellContext<RowData, number>) => {
+  return <>{ formatStake(ctx.getValue()) }</>;
+}
+
+const columnHelper = createColumnHelper<RowData>();
 
 const Atlas = () => {
+
+  const { isLoading, data: gateways } = useGateways();
+  const [tableData, setTableData] = useState<Array<RowData>>([]);
+
+  const columns = useMemo<ColumnDef<RowData, any>[]>(
+    () => [
+      columnHelper.accessor('label', {
+        id: 'label',
+        header: 'Label',
+        sortDescFirst: false,
+      }),
+      columnHelper.accessor('domain', {
+        id: 'domain',
+        header: 'Domain',
+        sortDescFirst: false,
+        cell: (ctx) => (
+          <div className="flex items-center gap-2">
+            <a
+              href={`https://${ctx.getValue()}`}
+              target="_blank"
+              rel="noreferrer"
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+              className="text-gradient"
+            >
+              {ctx.getValue()}
+            </a>
+            <CopyButton textToCopy={ctx.getValue()} />
+          </div>
+        ),
+      }),
+      columnHelper.accessor('operatorStake', {
+        id: 'operatorStake',
+        header: 'Operator Stake',
+        sortDescFirst: true,
+        cell: renderStake,
+      }),
+      columnHelper.accessor('delegatedStake', {
+        id: 'delegatedStake',
+        header: 'Delegated Stake',
+        sortDescFirst: true,
+        cell: renderStake,
+      }),
+      columnHelper.accessor('totalStake', {
+        id: 'totalStake',
+        header: 'Total Stake',
+        sortDescFirst: true,
+        cell: renderStake,
+      }),
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const tableData: Array<RowData> = Object.values(gateways ?? {}).map(
+      (gateway) => {
+        const totalStake = gateway.operatorStake + gateway.totalDelegatedStake
+        return {
+          label: gateway.settings.label,
+          domain: gateway.settings.fqdn,
+          operatorStake: new mARIOToken(gateway.operatorStake)
+            .toARIO()
+            .valueOf(),
+          delegatedStake: new mARIOToken(gateway.totalDelegatedStake)
+            .toARIO()
+            .valueOf(),
+          totalStake: new mARIOToken(totalStake)
+            .toARIO()
+            .valueOf(),
+        }
+      }
+    );
+    setTableData(tableData);
+  }, [gateways]);
+
   return (
     <div className="flex max-w-full flex-col gap-6">
       <Header />
-      Hello world
+      <div className="mb-8">
+        <div className="flex w-full items-center rounded-t-xl border border-grey-600 bg-containerL3 py-[0.9375rem] pl-6 pr-[0.8125rem]">
+          <div className="grow text-sm text-mid">Gateways</div>
+        </div>
+        <TableView
+          columns={columns}
+          data={tableData}
+          defaultSortingState={{ id: 'totalStake', desc: true }}
+          isLoading={isLoading}
+          noDataFoundText="Unable to fetch gateways."
+        />
+      </div>
     </div>
   )
 };

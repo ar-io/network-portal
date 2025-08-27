18d53cb29c5728cada5a2c2474cb63d14139ec5d
Steven Yi <stevenyi@gmail.com>
2025-08-27 14:19:38 -0400

    fix: margin bottom

--- src/pages/Observers/index.tsx
+++ src/pages/Observers/index.tsx
@@ -10,7 +10,7 @@ const Observers = () => {
       </div>
       <div className="flex-1 overflow-hidden">
         <div className="h-full overflow-y-auto">
-          <div className="flex flex-col gap-6 pt-0">
+          <div className="mb-6 flex flex-col gap-6 pt-0">
             <Banner />
             <ObserversTable />
           </div>

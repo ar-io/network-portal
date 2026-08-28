76ebd07046e61df33825a3f3ed3e8a99033d18e8
vilenarios <vilenarios@users.noreply.github.com>
2026-08-28 15:28:19 +0000

    deploy: 0614fb5f3a1b5b97ba842a12a60857fd8db26a07

---
+++ assets/chevron-down-cr4xPglP.js
@@ -0,0 +1,6 @@
+import{i as o}from"./index-qTgf14T-.js";/**
+ * @license lucide-react v0.461.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=o("ChevronDown",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]);export{n as C};
